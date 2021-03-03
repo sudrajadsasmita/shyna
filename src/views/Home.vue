c0c2a95034ac09395f21bed6a09b806f51809b8a
sudrajadsasmita <sudrajad.sasmita@gmail.com>
2021-03-03 13:52:03 +0700

    commit shyna

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShyna />
+    <HeroShyna />
+    <WomenShyna />
+    <InstaShyna />
+    <PartnerShyna />
+    <FooterShyna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShyna from "@/components/HeaderShyna.vue";
+import HeroShyna from "@/components/HeroShyna.vue";
+import WomenShyna from "@/components/WomenShyna.vue";
+import InstaShyna from "@/components/InstaShyna.vue";
+import PartnerShyna from "@/components/PartnerShyna.vue";
+import FooterShyna from "@/components/FooterShyna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShyna,
+    HeroShyna,
+    WomenShyna,
+    InstaShyna,
+    PartnerShyna,
+    FooterShyna,
+  },
+};
+</script>
